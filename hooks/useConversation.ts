bc4167df2a2c193d85ac0bb84f1b5e805fabbdf6
Tatyana Tupalo <tatyana080489t@gmail.com>
2023-11-28 17:47:48 +0200

    fix error deploy

--- hooks/useConversation.ts
+++ hooks/useConversation.ts
@@ -1,20 +1,23 @@
 import { useParams } from "next/navigation";
-import {useMemo} from 'react'
+import { useMemo } from "react";
 
 const useConversation = () => {
-    const params = useParams();
-    const conversationId = useMemo(() => {
-        if(!params?.conversationId) {
-            return ''
-        }
-        return params.conversationId as string
-    }, [params.conversationId])
+  const params = useParams();
 
-    const isOpen = useMemo(() => !!conversationId, [conversationId])
-    return useMemo(() =>({
-        isOpen,
-        conversationId
-    }), [conversationId, isOpen])
-}
+  const conversationId = useMemo(() => {
+    if (!params?.conversationId) {
+      return '';
+    }
 
-export default useConversation
+    return params.conversationId as string;
+  }, [params?.conversationId]);
+
+  const isOpen = useMemo(() => !!conversationId, [conversationId]);
+
+  return useMemo(() => ({
+    isOpen,
+    conversationId
+  }), [isOpen, conversationId]);
+};
+
+export default useConversation;
