5b3e5b682712b7a0a4e8627463f281f34edb320c
Tatyana Tupalo <tatyana080489t@gmail.com>
2024-08-09 10:33:39 +0300

    add google analitics tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,12 @@
-import './globals.css'
-import type { Metadata } from 'next'
+
+import { GoogleTagManager } from '@next/third-parties/google'
 import {AuthProvider} from "../context/Providers"
 import { Inter } from 'next/font/google'
 import NavBar from '../components/NavBar/NavBar'
 import Footer from '@/components/Footer'
 import ToasterContext from '../context/ToasterContext'
 import ActiveStatus from '../components/Chat/ActiveStatus'
+import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -19,6 +20,7 @@ export default function RootLayout({
     <html lang="uk">
       <head>
       <link rel="icon" href="/logo.png" />
+      <GoogleTagManager gtmId="G-HLDDJKXJP7" />
       </head>
       <body className={inter.className}>
         <AuthProvider>
